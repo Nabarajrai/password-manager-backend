528023ace1e55188e0b80afa40b17f8a94a901df
Nabarajrai <nabaraj2055@gmail.com>
2025-09-26 08:46:45 +0545

    fix(category): change category id to category_id based on database field name

--- src/router/categories.js
+++ src/router/categories.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { getAllCategories } from "../controllerr/categories.js";
+import {
+  getAllCategories,
+  updateCategory,
+  deleteCategory,
+  createCategory,
+} from "../controllerr/categories.js";
 const router = express.Router();
 
 // Define your category routes here
 router.get("/get-all-categories", getAllCategories);
+router.put("/update-category/:id", updateCategory);
+router.delete("/delete-category/:id", deleteCategory);
+router.post("/create-category", createCategory);
 
 export default router;
